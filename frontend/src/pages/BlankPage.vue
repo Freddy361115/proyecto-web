b7de562ef1663a0effaf5ed1e878942d5eb6cf70
Freddy361115 <tepazfreddyana@gmail.com>
2022-09-22 01:02:07 -0600

    Depurando template
    Templete base

---
+++ frontend/src/pages/BlankPage.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="content">
+    <div class="md-layout">
+      <div
+        class="md-layout-item md-medium-size-100 md-xsmall-size-100 md-size-100"
+      >
+        <md-card>
+          <md-card-header data-background-color="green">
+            <h4 class="title">Simple blankPage</h4>
+            <p class="category">Page description</p>
+          </md-card-header>
+          <md-card-content>
+          </md-card-content>
+        </md-card>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+};
+</script>
--- frontend/src/pages/Layout/DashboardLayout.vue
+++ frontend/src/pages/Layout/DashboardLayout.vue
@@ -8,32 +8,20 @@
     >
       <mobile-menu slot="content"></mobile-menu>
       <sidebar-link to="/dashboard">
-        <md-icon>dashboard</md-icon>
-        <p>Dashboard</p>
+        <md-icon>home</md-icon>
+        <p>Inicio</p>
       </sidebar-link>
-      <sidebar-link to="/user">
-        <md-icon>person</md-icon>
-        <p>User Profile</p>
+      <sidebar-link to="/blank">
+        <md-icon >person</md-icon>
+        <p>Profesores</p>
       </sidebar-link>
-      <sidebar-link to="/table">
-        <md-icon>content_paste</md-icon>
-        <p>Table list</p>
+      <sidebar-link to="/blank">
+        <md-icon>domain</md-icon>
+        <p>Establecimientos</p>
       </sidebar-link>
-      <sidebar-link to="/typography">
-        <md-icon>library_books</md-icon>
-        <p>Typography</p>
-      </sidebar-link>
-      <sidebar-link to="/icons">
-        <md-icon>bubble_chart</md-icon>
-        <p>Icons</p>
-      </sidebar-link>
-      <sidebar-link to="/maps">
-        <md-icon>location_on</md-icon>
-        <p>Maps</p>
-      </sidebar-link>
-      <sidebar-link to="/notifications">
-        <md-icon>notifications</md-icon>
-        <p>Notifications</p>
+      <sidebar-link to="/blank">
+        <md-icon>book</md-icon>
+        <p>Supervisores</p>
       </sidebar-link>
     </side-bar>
 

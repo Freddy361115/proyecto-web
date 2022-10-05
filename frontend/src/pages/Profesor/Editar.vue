f6bec945a0d50f259b61aa5f696aad828b26f8cc
Freddy361115 <tepazfreddyana@gmail.com>
2022-10-05 01:19:06 -0600

    Agregar establecimiento a CRUD profesores

--- frontend/src/pages/Profesor/Editar.vue
+++ frontend/src/pages/Profesor/Editar.vue
@@ -1,120 +1,147 @@
 <template>
-    <div class="content">
-      <div class="md-layout">
-        <div
-          class="md-layout-item md-medium-size-100 md-xsmall-size-100 md-size-100"
-        >
-          <md-card>
-            <md-card-header data-background-color="green">
-              <h4 class="title">Editar Profesor</h4>
-            </md-card-header>
-            <md-card-content>
-        <div class="md-layout" v-if="profesor">
-          <div class="md-layout-item md-small-size-100 md-size-33">
-            <md-field>
-              <label>Nombres</label>
-              <md-input required v-model="profesor.nombres"></md-input>
-            </md-field>
-          </div>
-          <div class="md-layout-item md-small-size-100 md-size-33">
-            <md-field>
-              <label>Apellidos</label>
-              <md-input required v-model="profesor.apellidos" type="text"></md-input>
-            </md-field>
-          </div>
-          <div class="md-layout-item md-small-size-100 md-size-33">
-            <md-field>
-              <label>Fecha Nacimiento</label>
-              <md-input required v-model="profesor.fecha_nacimiento" type="date"></md-input>
-            </md-field>
-          </div>
-          <div class="md-layout-item md-small-size-100 md-size-33">
-            <md-field>
-              <label>Nombre de Usuario</label>
-              <md-input required v-model="profesor.nombre_usuario" type="text"></md-input>
-            </md-field>
-          </div>
-          <div class="md-layout-item md-small-size-100 md-size-33">
-            <md-field>
-              <label>Email</label>
-              <md-input required v-model="profesor.email" type="email"></md-input>
-            </md-field>
-          </div>
-          <div class="md-layout-item md-small-size-100 md-size-33">
-            <md-field>
-              <label>Password</label>
-              <md-input required v-model="profesor.password" type="password"></md-input>
-            </md-field>
-          </div>
-          <div class="md-layout-item md-small-size-100 md-size-33">
-            <md-field>
-              <label>DPI</label>
-              <md-input v-model="profesor.dpi" maxlength="13" type="text"></md-input>
-            </md-field>
-          </div>
-          <div class="md-layout-item md-small-size-100 md-size-33">
-            <md-field>
-              <label>Direccion</label>
-              <md-input v-model="profesor.direccion" type="text"></md-input>
-            </md-field>
-          </div>
-          <div class="md-layout-item md-small-size-100 md-size-33">
-            <md-field>
-              <label>Telefono</label>
-              <md-input v-model="profesor.telefono" maxlength="8" type="text"></md-input>
-            </md-field>
-          </div>
-          <div class="md-layout-item md-size-100 text-right">
-            <md-button class="md-raised md-success" @click="save">Guardar</md-button>
-            <md-button class="md-raised md-default" @click="goBack">Cancelar</md-button>
-          </div>
+<div class="content">
+    <div class="md-layout">
+        <div class="md-layout-item md-medium-size-100 md-xsmall-size-100 md-size-100">
+            <md-card>
+                <md-card-header data-background-color="green">
+                    <h4 class="title">Editar Profesor</h4>
+                </md-card-header>
+                <md-card-content>
+                    <div class="md-layout" v-if="profesor">
+                        <div class="md-layout-item md-small-size-100 md-size-33">
+                            <md-field>
+                                <label>Nombres</label>
+                                <md-input required v-model="profesor.nombres"></md-input>
+                            </md-field>
+                        </div>
+                        <div class="md-layout-item md-small-size-100 md-size-33">
+                            <md-field>
+                                <label>Apellidos</label>
+                                <md-input required v-model="profesor.apellidos" type="text"></md-input>
+                            </md-field>
+                        </div>
+                        <div class="md-layout-item md-small-size-100 md-size-33">
+                            <md-field>
+                                <label>Fecha Nacimiento</label>
+                                <md-input required v-model="profesor.fecha_nacimiento" type="date"></md-input>
+                            </md-field>
+                        </div>
+                        <div class="md-layout-item md-small-size-100 md-size-33">
+                            <md-field>
+                                <label>Nombre de Usuario</label>
+                                <md-input required v-model="profesor.nombre_usuario" type="text"></md-input>
+                            </md-field>
+                        </div>
+                        <div class="md-layout-item md-small-size-100 md-size-33">
+                            <md-field>
+                                <label>Email</label>
+                                <md-input required v-model="profesor.email" type="email"></md-input>
+                            </md-field>
+                        </div>
+                        <div class="md-layout-item md-small-size-100 md-size-33">
+                            <md-field>
+                                <label>Password</label>
+                                <md-input required v-model="profesor.password" type="password"></md-input>
+                            </md-field>
+                        </div>
+                        <div class="md-layout-item md-small-size-100 md-size-33">
+                            <md-field>
+                                <label>DPI</label>
+                                <md-input v-model="profesor.dpi" maxlength="13" type="text"></md-input>
+                            </md-field>
+                        </div>
+                        <div class="md-layout-item md-small-size-100 md-size-33">
+                            <md-field>
+                                <label>Direccion</label>
+                                <md-input v-model="profesor.direccion" type="text"></md-input>
+                            </md-field>
+                        </div>
+                        <div class="md-layout-item md-small-size-100 md-size-33">
+                            <md-field>
+                                <label>Telefono</label>
+                                <md-input v-model="profesor.telefono" maxlength="8" type="text"></md-input>
+                            </md-field>
+                        </div>
+                        <div class="md-layout-item md-small-size-100 md-size-33">
+                            <span>Establecimiento</span>
+                            <md-field>
+                                <v-select :loading="loading" :options="establecimientos" style="width:100%" type="text" name="establecimiento" class="rounded" v-model="establecimiento_selected">
+                                    <template #no-options>
+                                        No hay items disponibles
+                                    </template>
+                                </v-select>
+                            </md-field>
+                        </div>
+                        <div class="md-layout-item md-size-100 text-right">
+                            <md-button class="md-raised md-success" @click="save">Guardar</md-button>
+                            <md-button class="md-raised md-default" @click="goBack">Cancelar</md-button>
+                        </div>
+                    </div>
+                </md-card-content>
+            </md-card>
         </div>
-      </md-card-content>
-          </md-card>
-        </div>
-      </div>
     </div>
-  </template>
+</div>
+</template>
 
-  <script>
+<script>
+export default {
+    name: "edit-profile-form",
+    props: {
+        dataBackgroundColor: {
+            type: String,
+            default: "",
+        },
+    },
+    data() {
+        return {
+            profesor: null,
+            establecimiento_data: null,
+            establecimiento_selected: null,
+            loading: false,
+            establecimientos: [],
+        };
+    },
+    async mounted() {
+        let that = this;
+        this.loading = true;
+        this.establecimiento_data = await this.$getRequest(process.env.VUE_APP_API + '/establecimientos');
+        this.establecimiento_data.forEach(sup => {
+            that.establecimientos.push({
+                label: sup.nombre,
+                code: sup.id_establecimiento
+            });
+        });
 
-  export default {
-  name: "edit-profile-form",
-  props: {
-    dataBackgroundColor: {
-      type: String,
-      default: "",
-    },
-  },
-  data() {
-    return {
-      profesor: null
-    };
-  },
-  async mounted() {
-    this.profesor = await this.$getRequest(process.env.VUE_APP_API + '/profesores/' + this.$route.params.id);
-  },
-  methods: {
-    goBack() {
-    //   this.$router.replace({ path: "/municipalities" });
-      this.$router.push({
-            name: "Profesores"
-          });
-    },
-    async save() {
-      let that = this;
-      let response = await this.$putRequest(process.env.VUE_APP_API + "/profesores/" + this.profesor.id, this.profesor);
-      if (response.success) {
-        if(!that.file)
-        {
-        //   this.$createdSuccess();
-        //   that.resource_id = response.resource_id;
-          this.$router.push({
-            name: "Profesores"
-          });
+        this.profesor = await this.$getRequest(process.env.VUE_APP_API + '/profesores/' + this.$route.params.id);
+
+        if (this.profesor.id_establecimiento) {
+            this.establecimiento_selected = this.establecimientos.find(x => x.code == this.profesor.id_establecimiento);
         }
-      }
+
+        this.loading = false;
     },
-  },
+    methods: {
+        goBack() {
+            //   this.$router.replace({ path: "/municipalities" });
+            this.$router.push({
+                name: "Profesores"
+            });
+        },
+        async save() {
+            let that = this;
+            this.profesor.id_establecimiento = this.establecimiento_selected.code;
+            let response = await this.$putRequest(process.env.VUE_APP_API + "/profesores/" + this.profesor.id, this.profesor);
+            if (response.success) {
+                if (!that.file) {
+                    //   this.$createdSuccess();
+                    //   that.resource_id = response.resource_id;
+                    this.$router.push({
+                        name: "Profesores"
+                    });
+                }
+            }
+        },
+    },
 };
-  </script>
+</script>

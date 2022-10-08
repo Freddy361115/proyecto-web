5d4e7542a6407f5d87e7322554bfe22f2689bb35
Fredy <tepazfreddyana@gmail.com>
2022-10-08 16:23:22 -0600

    corrigiendo rutas.

--- routes/api.php
+++ routes/api.php
@@ -20,7 +20,22 @@ Route::get('details/{user}', 'App\Http\Controllers\UserController@details');
 
 //Route::get('notifiacion/{user}/getNotificationbyUser', 'App\Http\Controllers\NotificacionController@getNotificationsbyUser');
 
+/*
+Route::resource('profesores','App\Http\Controllers\ProfesorController');
+Route::resource('supervisors','App\Http\Controllers\SupervisorController');
+Route::resource('establecimientos','App\Http\Controllers\EstablecimientoController');
+Route::resource('grados','App\Http\Controllers\GradoController');
+Route::resource('roles','App\Http\Controllers\RoleController');
+Route::resource('tiponotificacion','App\Http\Controllers\TipoNotificacionController');
+Route::resource('notificacion','App\Http\Controllers\NotificacionController');
+Route::get('misnotificaciones/{id}','App\Http\Controllers\NotificacionController@misnotificaciones');
+Route::get('misnotificacionesleidas/{id}','App\Http\Controllers\NotificacionController@misnotificacionesleidas');
+Route::get('misnotificacionesnoleidas/{id}','App\Http\Controllers\NotificacionController@misnotificacionesnoleidas');
+*/
 
+
+Route::group(['middleware' => 'auth:api'], function(){
+Route::post('details', 'App\Http\Controllers\UserController@details');
 Route::resource('profesores','App\Http\Controllers\ProfesorController');
 Route::resource('supervisors','App\Http\Controllers\SupervisorController');
 Route::resource('establecimientos','App\Http\Controllers\EstablecimientoController');
@@ -32,15 +47,5 @@ Route::get('misnotificaciones/{id}','App\Http\Controllers\NotificacionController
 Route::get('misnotificacionesleidas/{id}','App\Http\Controllers\NotificacionController@misnotificacionesleidas');
 Route::get('misnotificacionesnoleidas/{id}','App\Http\Controllers\NotificacionController@misnotificacionesnoleidas');
 
-/*
-Route::group(['middleware' => 'auth:api'], function(){
-Route::post('details', 'App\Http\Controllers\UserController@details');
-Route::resource('profesores','App\Http\Controllers\ProfesorController');
-Route::resource('supervisors','App\Http\Controllers\SupervisorController');
-Route::resource('establecimientos','App\Http\Controllers\EstablecimientoController');
-Route::resource('grados','App\Http\Controllers\GradoController');
-Route::resource('roles','App\Http\Controllers\RoleController');
-Route::resource('tiponotificacion','App\Http\Controllers\TipoNotificacionController');
-Route::resource('notificacion','App\Http\Controllers\NotificacionController');
 });
-*/
+
